4f0a56e535fa0ecbc827dfbae2af897de53830de
VJ13SS <viswajith5749@gmail.com>
2025-06-04 15:11:02 +0530

    updated environment variables

--- server/config/db.js
+++ server/config/db.js
@@ -4,7 +4,6 @@ import mongoose from "mongoose";
 const connectDb = async () => {
     mongoose.connection.on('connected',()=>console.log('DB Connecetd'))
 
-    //await mongoose.connect('mongodb://localhost:27017/find-it')
     await mongoose.connect(process.env.DATABASE_URL)
 }
 
